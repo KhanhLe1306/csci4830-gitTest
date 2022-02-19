a692bd8ca728bba378db685f07c4df01907229b5
khanh <khanhle@unomaha.edu>
2022-02-19 08:54:14 -0800

    Changed after the first commit!

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -27,7 +27,7 @@ public class Hello extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hello motherfucker!");
+		response.getWriter().append("Hello motherfucker! THis is the change after the first commit");
 	}
 
 	/**
